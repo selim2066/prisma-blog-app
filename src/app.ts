0dbc5477621eeb2d34d19cad1aa922d5d74d9d11
selim2066 <mdselimreza2066@gmail.com>
2026-01-02 14:42:36 +0600

    alhamdulillah. generate and migrate problem solved

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,10 @@
 import express, { Application } from "express";
 import { PostRouter } from "./module/post/post.router";
+import { auth } from "./lib/auth";
+import { toNodeHandler } from "better-auth/node";
 
 const app: Application = express();
+app.all("/api/auth/*splat", toNodeHandler(auth));
 
 app.use(express.json());
 app.use("/post", PostRouter);
