1584519dd9c23003e1c9193d91278f7ed355284f
selim2066 <mdselimreza2066@gmail.com>
2026-01-05 12:01:09 +0600

    dynamic author id set

--- src/module/post/post.controller.ts
+++ src/module/post/post.controller.ts
@@ -3,8 +3,12 @@ import { postService } from "./post.service";
 
 const createPost = async (req: Request, res: Response) => {
   try {
-    console.log(req.user)
-    const result = await postService.createPost(req.body);
+    //console.log(req.user)
+    const user = req.user;
+    if (!user) {
+      return res.status(401).json({ message: "Unauthorized" });
+    }
+    const result = await postService.createPost(req.body, user.id as string);
     res.status(201).json(result);
   } catch (error) {
     res.status(400).json({
