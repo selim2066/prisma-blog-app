3f693da680f538fe648d44ab6767e39ae0e3d932
selim2066 <mdselimreza2066@gmail.com>
2026-01-13 01:05:57 +0600

    checked and signUpAdmin created

--- src/scripts/seedAdmin.ts
+++ src/scripts/seedAdmin.ts
@@ -2,13 +2,12 @@ import { prisma } from "../lib/prisma";
 import { UserRole } from "../middleware/authMiddleware";
 
 async function seedAdmin() {
-
   // admin data
   const adminData = {
-    name: 'adminAmi',
-    email: 'selim@gmail.com',
-    password:'admin123',
-    role: UserRole.ADMIN
+    name: "adminAmi",
+    email: "selim@gmail.com",
+    password: "admin123",
+    role: UserRole.ADMIN,
   };
 
   // check if admin already exists
@@ -17,7 +16,44 @@ async function seedAdmin() {
   });
 
   if (existingAdmin) {
-    throw new Error('Admin user already exists. Skipping seeding.');
-  
+    throw new Error("Admin user already exists. Skipping seeding.");
+  }
+
+  if (!existingAdmin) {
+    // create admin user
+    const signUpAdmin = await fetch(
+      "http://localhost:3001/api/auth/sign-up/email",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(adminData),
+      }
+    );
+
+    // ! Explanation:
+    /* This code is used to create (sign up) an admin user by calling your backend API.
+
+    Specifically:
+
+    It sends admin data (email, password, role, etc.)
+
+    To your backend endpoint:
+    POST /api/auth/sign-up/email
+
+    The backend then:
+
+    # Validates data
+
+    # Hashes password
+
+    # Saves admin in the database */
+
+    if (!signUpAdmin.ok) {
+      throw new Error("Failed to create admin user.");
+    }
+
+    console.log("Admin user created successfully.");
   }
 }
