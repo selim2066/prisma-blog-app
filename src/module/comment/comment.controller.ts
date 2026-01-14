745f6477bac3ce2c9e06c94eef01dba187ee170a
selim2066 <mdselimreza2066@gmail.com>
2026-01-14 21:36:52 +0600

    comment creation part 2

--- src/module/comment/comment.controller.ts
+++ src/module/comment/comment.controller.ts
@@ -1,21 +1,22 @@
 import { Request, Response } from "express";
 import { CommentService } from "./comment.service";
 
-const createCommentController = async(req:Request, res:Response) => {
-
+const createCommentController = async (req: Request, res: Response) => {
   try {
-    req.body.authorId = req.user?.id;
+    req.body.authorId = req.user?.id; //!how i get user id from auth middleware
     const resultComment = await CommentService.createCommentService(req.body);
-    
+    return res.status(201).json({
+      message: "Comment created successfully",
+      data: resultComment,
+    });
   } catch (error) {
     res.status(400).json({
       error: "comment creation failed",
       details: error,
     });
   }
-
-}
+};
 
 export const CommentController = {
-    createCommentController
-}
+  createCommentController,
+};
--- src/module/comment/comment.service.ts
+++ src/module/comment/comment.service.ts
@@ -1,3 +1,5 @@
+import { prisma } from "../../lib/prisma";
+
 const createCommentService = async (payload: {
   postId: string;
   authorId: string;
@@ -5,6 +7,25 @@ const createCommentService = async (payload: {
   parentId?: string;
 }) => {
   console.log("create comment service", payload);
+
+  // condition before creating comment
+  // Check if the post exists
+  const post = await prisma.post.findUniqueOrThrow({
+    where: { id: payload.postId },
+  });
+  
+
+  // Check if this comment is a reply
+  if(payload.parentId){
+    const parentComment = await prisma.comment.findUniqueOrThrow({
+      where: {id: payload.parentId}
+    })
+  }
+
+  // create comment in database
+  return await prisma.comment.create({
+    data:payload
+  })
 };
 
 export const CommentService = {
