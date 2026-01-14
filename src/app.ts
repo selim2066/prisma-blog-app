ef157e2a76d2126f0fe9f2378f8a23bf3b30b7d1
selim2066 <mdselimreza2066@gmail.com>
2026-01-14 18:43:32 +0600

    alhamdulillah new module 27 have started. Comment router setup

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,8 @@ import { PostRouter } from "./module/post/post.router";
 import { auth } from "./lib/auth";
 import { toNodeHandler } from "better-auth/node";
 import cors from 'cors'
+import { CommentRouter } from "./module/comment/comment.router";
+
 
 const app: Application = express();
 app.all("/api/auth/*splat", toNodeHandler(auth));
@@ -12,8 +14,13 @@ app.use(cors({
   origin: process.env.APP_URL || "http://localhost:3000",
   credentials: true,
 }))
+
+// ! post router
 app.use("/post", PostRouter);
 
+// ! comment router
+
+app.use("/comment",CommentRouter)
 
 app.get("/", (req, res) => {
   res.send("Hello, World!");
